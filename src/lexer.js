02e5bfda9ce6c66bb74abb086428204a742a08e1
denzp <denys.zariaiev@gmail.com>
2014-02-13 01:29:37 +0200

    lexer progress

--- src/lexer.js
+++ src/lexer.js
@@ -7,11 +7,17 @@ function Lexer(buffer) {
   this.offset = 0;
 }
 
-var WHITESPACE = /\s/;
+var WHITESPACE  = /\s/,
+    S_DIGIT     = /[\d\.]/,
+    NUMBER      = /^(\d*\.\d+|\d+)$/,
+    S_LETTER    = /[\wа-я]/,
+    LITERAL     = /^[\wа-я]+$/,
+    OPERATOR    = /^(\+|\-|\*|\/)$/,
+    S_BRACKET   = /(\(|\[|\]|\))/,
+    BRACKET     = /^(\(|\[|\]|\))$/;
 
 Lexer.prototype.getNextToken = function() {
-  var length = this.buffer.length,
-      start;
+  var length = this.buffer.length;
   
   while(this.offset < length && WHITESPACE.test(this.buffer[this.offset])) {
     ++this.offset;
@@ -21,9 +27,25 @@ Lexer.prototype.getNextToken = function() {
     return;
   }
   
-  start = this.offset;
-  while(this.offset < length && !WHITESPACE.test(this.buffer[this.offset])) {
+  var start = this.offset;
+  
+  if(S_DIGIT.test(this.buffer[this.offset])) {
+    while(this.offset < length && S_DIGIT.test(this.buffer[this.offset])) {
+      ++this.offset;
+    }
+  }
+  else if(S_LETTER.test(this.buffer[this.offset])) {
+    while(this.offset < length && S_LETTER.test(this.buffer[this.offset])) {
+      ++this.offset;
+    }
+  }
+  else if(S_BRACKET.test(this.buffer[this.offset])) {
     ++this.offset;
+  }
+  else {
+    while(this.offset < length && !WHITESPACE.test(this.buffer[this.offset])) {
+      ++this.offset;
+    }
   }
   
   return {
@@ -33,6 +55,49 @@ Lexer.prototype.getNextToken = function() {
       end: this.offset
     }
   };
+};
+
+Lexer.prototype.getTokenType = function(token) {
+  if(!token || !token.text || token.loc.start === undefined || token.loc.end === undefined) {
+    throw new ReferenceError('getTokenType() failed: wrong input');
+  }
+  
+  if(NUMBER.test(token.text)) {
+    return {
+      type: 'number',
+      value: parseFloat(token.text),
+      loc: token.loc
+    };
+  }
+  
+  if(LITERAL.test(token.text)) {
+    return {
+      type: 'literal',
+      value: token.text,
+      loc: token.loc
+    };
+  }
+  
+  if(OPERATOR.test(token.text)) {
+    return {
+      type: 'operator',
+      value: token.text,
+      loc: token.loc
+    };
+  }
+  
+  if(BRACKET.test(token.text)) {
+    return {
+      type: 'bracket',
+      value: token.text,
+      loc: token.loc
+    };
+  }
+  
+  var error = new SyntaxError('invalid token: "' + token.text + '"');
+  error.loc = token.loc;
+  
+  throw error;
 };
 
 module.exports = Lexer;
--- test/lexer.js
+++ test/lexer.js
@@ -5,32 +5,99 @@
 var Lexer = require('../src/lexer.js');
 
 module.exports.getNextToken = function(test) {
-  var string = 'aaaa + бб   1',
+  var string = 'aaaa + бб   1(([',
       instance = new Lexer(string);
   
   var chunk = instance.getNextToken();
-  test.equal(chunk.text, 'aaaa');
-  test.equal(chunk.loc.start, 0);
-  test.equal(chunk.loc.end, 4);
+  test.strictEqual(chunk.text, 'aaaa');
+  test.strictEqual(chunk.loc.start, 0);
+  test.strictEqual(chunk.loc.end, 4);
   
   
   chunk = instance.getNextToken();
-  test.equal(chunk.text, '+');
-  test.equal(chunk.loc.start, 5);
-  test.equal(chunk.loc.end, 6);
+  test.strictEqual(chunk.text, '+');
+  test.strictEqual(chunk.loc.start, 5);
+  test.strictEqual(chunk.loc.end, 6);
   
   chunk = instance.getNextToken();
-  test.equal(chunk.text, 'бб');
-  test.equal(chunk.loc.start, 7);
-  test.equal(chunk.loc.end, 9);
+  test.strictEqual(chunk.text, 'бб');
+  test.strictEqual(chunk.loc.start, 7);
+  test.strictEqual(chunk.loc.end, 9);
   
   chunk = instance.getNextToken();
-  test.equal(chunk.text, '1');
-  test.equal(chunk.loc.start, 12);
-  test.equal(chunk.loc.end, 13);
+  test.strictEqual(chunk.text, '1');
+  test.strictEqual(chunk.loc.start, 12);
+  test.strictEqual(chunk.loc.end, 13);
   
   chunk = instance.getNextToken();
-  test.equal(chunk, undefined);
+  test.strictEqual(chunk.text, '(');
+  test.strictEqual(chunk.loc.start, 13);
+  test.strictEqual(chunk.loc.end, 14);
+  
+  chunk = instance.getNextToken();
+  test.strictEqual(chunk.text, '(');
+  test.strictEqual(chunk.loc.start, 14);
+  test.strictEqual(chunk.loc.end, 15);
+  
+  chunk = instance.getNextToken();
+  test.strictEqual(chunk.text, '[');
+  test.strictEqual(chunk.loc.start, 15);
+  test.strictEqual(chunk.loc.end, 16);
+  
+  chunk = instance.getNextToken();
+  test.strictEqual(chunk, undefined);
+  
+  test.done();
+};
+
+module.exports.getTokenType = function(test) {
+  var string = 'aaaa + бб   1 ((] ++',
+      instance = new Lexer(string);
+  
+  var token = instance.getNextToken(),
+      type  = instance.getTokenType(token);
+  
+  test.strictEqual(type.type, 'literal');
+  test.strictEqual(type.value, 'aaaa');
+  
+  token = instance.getNextToken();
+  type  = instance.getTokenType(token);
+  test.strictEqual(type.type, 'operator');
+  test.strictEqual(type.value, '+');
+  
+  token = instance.getNextToken();
+  type  = instance.getTokenType(token);
+  test.strictEqual(type.type, 'literal');
+  test.strictEqual(type.value, 'бб');
+  
+  token = instance.getNextToken();
+  type  = instance.getTokenType(token);
+  test.strictEqual(type.type, 'number');
+  test.strictEqual(type.value, 1);
+  
+  token = instance.getNextToken();
+  type  = instance.getTokenType(token);
+  test.strictEqual(type.type, 'bracket');
+  test.strictEqual(type.value, '(');
+  
+  token = instance.getNextToken();
+  type  = instance.getTokenType(token);
+  test.strictEqual(type.type, 'bracket');
+  test.strictEqual(type.value, '(');
+  
+  token = instance.getNextToken();
+  type  = instance.getTokenType(token);
+  test.strictEqual(type.type, 'bracket');
+  test.strictEqual(type.value, ']');
+  
+  token = instance.getNextToken();
+  test.throws(function() {
+    instance.getTokenType(token);
+  }, SyntaxError);
+  
+  test.throws(function() {
+    instance.getTokenType();
+  }, ReferenceError);
   
   test.done();
 };
