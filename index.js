855a2e3c5f8aad4448e77298b2f102810e72b529
Denys Zariaiev <denys.zariaiev@gmail.com>
2014-02-13 19:49:10 +0200

    integration test

---
+++ index.js
@@ -0,0 +1,5 @@
+/*jslint white: true, node: true, plusplus: true, vars: true */
+/*global module, require, __dirname */
+'use strict';
+
+module.exports.Lexer = require(__dirname + '/src/lexer.js');
---
+++ test/api.js
@@ -0,0 +1,11 @@
+/*jslint white: true, node: true, plusplus: true, vars: true */
+/*global module, require */
+'use strict';
+
+var symath = require('../index.js');
+
+module.exports.integrationTest = function(test) {
+  test.ok(symath.Lexer);
+  
+  test.done();
+};
